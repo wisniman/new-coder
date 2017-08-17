83e04539e5bb1f99f15057bb50f504aaa5e6061a
wisniman <manuel.wisniewski@gmail.com>
2017-08-17 19:20:06 +0200

    file:   dataviz_workspace/data/sample_sfpd_incident_all.csv

--- dataviz_workspace/parse.py
+++ dataviz_workspace/parse.py
@@ -13,43 +13,37 @@ Distributed under the zlib png license. See LICENSE for details.
 
 import csv
 
-
-# Put the full path to your CSV/Excel file here
-MY_FILE = "../data/sample_sfpd_incident_all.csv"
+MY_FILE = "./data/sample_sfpd_incident_all.csv"
 
 
 def parse(raw_file, delimiter):
-    """Parses a raw CSV file to a JSON-like object"""
+    """Parses a raw CSV file to a JSON-line object."""
 
-    # Open CSV file, and safely close it when we're done
+    # Open CSV file
     opened_file = open(raw_file)
 
-    # Read the CSV data
+    # Read CSV file
     csv_data = csv.reader(opened_file, delimiter=delimiter)
 
-    # Setup an empty list
+    # Build a data structure to return parsed_data
     parsed_data = []
-
-    # Skip over the first line of the file for the headers
     fields = csv_data.next()
-
-    # Iterate over each row of the csv file, zip together field -> value
     for row in csv_data:
         parsed_data.append(dict(zip(fields, row)))
 
-    # Close the CSV file
+    # Close CSV file
     opened_file.close()
 
     return parsed_data
 
 
 def main():
-    # Call our parse function and give it the needed parameters
+    # Call the parse function and give it the needed parameters
     new_data = parse(MY_FILE, ",")
 
-    # Let's see what the data looks like!
-    print new_data
+    # Print the data
+    print(new_data)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
